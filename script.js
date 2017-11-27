dceee1469e9a49ac5836c1290509377a57899d37
Matthew Fitch <matthewfitch@Matthews-MacBook-Pro.local>
2017-11-27 23:55:45 +0000

    Add HTTP request to fpl api, display last week's score for player 500 on all players, update styling of stats

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var playerCount = 0;
 var remainingAttempts = 25;
 var timeout = 1000;
 var playerDivClass = 'ism-element';
+var apiUrl = "https://fantasy.premierleague.com/drf/element-summary/";
 
 function havePlayersLoaded() {
     playerCount = document.getElementsByClassName(playerDivClass).length;
@@ -25,8 +26,11 @@ function main() {
 
     console.log(playerCount + ' players found.');
 
-    players.forEach(element => {
-        element.appendChild(createStatsElement('test'))
+    var client = new HttpClient();
+    client.get(apiUrl + "500", function(result) {
+        players.forEach(element => {
+            element.appendChild(createStatsElement(parseResult(result)));
+        });
     });
 }
 
@@ -34,8 +38,30 @@ function createStatsElement(playerName) {
     var div = document.createElement('div');
     var content = document.createTextNode(playerName);
     div.appendChild(content);
+    div.style = "background-color:#00FF87;";
 
     return div;
+}
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );
+        anHttpRequest.send( null );
+    }
+}
+
+function parseResult(result) {
+    var json = JSON.parse(result);
+    var summary = json["history_summary"];
+    var points = summary[summary.length - 1]["total_points"]
+    console.log(points);
+    return points;
 }
 
 havePlayersLoaded();
